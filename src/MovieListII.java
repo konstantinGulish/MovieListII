8b75d6abdfd5ba29a15ab03d49491b0f845d0af5
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-08 15:55:35 -0400

    changed the logic to validate user input against the elements already in the array

--- src/MovieListII.java
+++ src/MovieListII.java
@@ -2,25 +2,47 @@ import java.util.Scanner;
 public class MovieListII {
 
     public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	String [] movies = new String [100];
-	final String SENTINEL = "q";
-	String userInput = "";
-	int index = 0;
+	    // Create a scanner object
+        Scanner in = new Scanner(System.in);
 
-	while (!userInput.equals(SENTINEL)){
-        System.out.println("Enter the name of your favorite movie (press q to quit): ");
-        userInput = in.nextLine();
-        if (!userInput.toLowerCase().equals(SENTINEL)) {
-            movies[index] = userInput;
-            index++;
+        // Declare the necessary variables
+        String movie;
+        String [] movies = new String [100];
+        final String SENTINEL = "q";
+        String userInput = "";
+
+        // Initialize the index to the first element in the array
+        int index = 0;
+
+
+        while (!userInput.equals(SENTINEL)){
+
+            // Prompt and read-in movie title
+            System.out.println("Enter the name of your favorite movie (press q to quit): ");
+            userInput = in.nextLine();
+            if (!userInput.equalsIgnoreCase(SENTINEL)) {
+                movie = userInput;
+                movies[index] = movie;
+
+                // Validate that the title entered is not already in the array
+                for (int i = 0; i < index; i++) {
+                    while (movies[i].equalsIgnoreCase(movie)) {
+                        System.out.println("This movie exists in the list. Enter another movie:");
+                        movie = in.nextLine();
+                        movies[index] = movie;
+                    }
+                }
+
+                // Increase the index to the next place in the array
+                index++;
+            }
+            else {
+                userInput = SENTINEL;
+            }
         }
-        else {
-            userInput = SENTINEL;
-        }
-    }
-    for (int i = 0; i < index; i++) {
-            System.out.println(movies[i]);
-        }
+        // Output the titles in the array to the screen
+            for (int i = 0; i < index; i++) {
+                    System.out.println(movies[i]);
+                }
     }
 }
